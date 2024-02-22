ec4d3dc63a7664edf1f087b1ffc1dadea7c5e18b
longquan98@gmail.com <longquan98@gmail.com>
2024-02-22 16:08:00 -0500

    Adding greeting message

--- src/main/java/com/vinay/spring/mvc/ServerController.java
+++ src/main/java/com/vinay/spring/mvc/ServerController.java
@@ -3,6 +3,7 @@ package com.vinay.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import java.time.LocalTime;
 
 @Controller
 public class ServerController {
@@ -11,6 +12,16 @@ public class ServerController {
     public ModelAndView home() 
 	{
 	    ModelAndView mview = new ModelAndView("index"); 
+		mview.addObject("greeting", getGreeting());
 	    return mview;
+    }
+
+	private String getGreeting() {
+        LocalTime currentTime = LocalTime.now();
+        if (currentTime.isBefore(LocalTime.NOON)) {
+            return "Good morning Hoang";
+        } else {
+            return "Good evening Hoang";
+        }
     }
 }
